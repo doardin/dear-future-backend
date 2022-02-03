86a94b38ba99a1736b504a114c0291ff7d3afc72
Eduardo Lopes <duh.lopes234@gmail.com>
2022-02-03 19:37:43 -0300

    Initial commit

---
+++ src/main/java/br/com/dearfuture/DearFutureApplication.java
@@ -0,0 +1,13 @@
+package br.com.dearfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearFutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dearfuture/DearFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dearfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
